294b686c4a29db7c0e1099acfc2358401800cf4a
awallace2 <anderswallace20@gmail.com>
2025-03-10 18:03:24 -0500

    Add more styling for homepage header and content

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -20,9 +20,14 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center min-h-screen mb-10">
-      <div className="w-full max-w-[1000px] px-4">
-        <div className="flex flex-col items-center gap-20">
+    <div className="flex flex-col items-center min-h-screen mb-10 mt-5">
+      <header className="w-full max-w-[1000px] bg-white fixed top-0 z-10 lg:py-10 sm:py-0 px-5 lg:block">
+        <h1 className="lg:text-4xl sm:text-1xl font-bold text-gray-900 mb-10">
+          Anders Wallace
+        </h1>
+      </header>
+      <div className="w-full max-w-[1000px] px-4 lg:pt-35 sm:pt-5">
+        <div className="flex flex-col items-start gap-20">
           {photos.map((photo) => (
             <PhotoImage
               key={photo.id}
---
+++ client/tailwing.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      fontFamily: {
+        helvetica: ["Helvetica", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
