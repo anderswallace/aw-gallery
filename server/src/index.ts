4c673840c2498e420528e28aeb77186eb108a043
awallace2 <anderswallace20@gmail.com>
2025-03-18 03:47:57 -0500

    Add rate limiting to API

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,26 @@ import cors from "cors";
 import { authenticateToken } from "./middleware/authMiddleware.js";
 import cookieParser from "cookie-parser";
 import { PrismaClient } from "@prisma/client";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = 4000;
 const prisma = new PrismaClient();
+
+const authRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min window
+  max: 10,
+  message: "Too many login attempts. Please try again later.",
+  headers: true,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const apiRateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 min window
+  max: 50,
+  message: "Too many requests. Please slow down.",
+});
 
 async function initializeServer() {
   try {
@@ -25,14 +41,9 @@ async function initializeServer() {
     app.use(cookieParser());
     app.use(express.json());
 
-    app.get("/", (req, res) => {
-      console.log("Test");
-      res.send("Hello from homepage");
-    });
-
-    app.use("/auth", authRoutes);
-    app.use("/photos", authenticateToken, photosRoutes);
-    app.use("/data", dataRoutes);
+    app.use("/auth", authRateLimiter, authRoutes);
+    app.use("/photos", authRateLimiter, authenticateToken, photosRoutes);
+    app.use("/data", apiRateLimiter, dataRoutes);
 
     app.listen(PORT, () => {
       console.log(`Server now running on port: http://localhost:${PORT}`);
